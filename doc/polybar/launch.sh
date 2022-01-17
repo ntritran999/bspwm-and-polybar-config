f26c3995dae386714591eed99c56bcfe9e5fee0d
just a random guy <81624097+ntritran999@users.noreply.github.com>
2022-01-17 21:40:56 +0700

    Update launch.sh

--- doc/polybar/launch.sh
+++ doc/polybar/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch top-bar and bottom-bar
 echo "---" | tee -a /tmp/top-bar.log /tmp/bottom-bar.log
 polybar top-bar 2>&1 | tee -a /tmp/top-bar.log & disown
 polybar bottom-bar 2>&1 | tee -a /tmp/bottom-bar.log & disown
