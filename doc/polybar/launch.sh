2782f1da77615cfdd3ab85ef295a936ccbb50415
just a random guy <81624097+ntritran999@users.noreply.github.com>
2022-01-17 20:57:25 +0700

    Create launch.sh

---
+++ doc/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/top-bar.log /tmp/bottom-bar.log
+polybar top-bar 2>&1 | tee -a /tmp/top-bar.log & disown
+polybar bottom-bar 2>&1 | tee -a /tmp/bottom-bar.log & disown
+
+echo "Bars launched..."
